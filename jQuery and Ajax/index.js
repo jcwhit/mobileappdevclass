a3c4c1d03480bc3ad6e938e75c754bfabe96b02d
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2016-06-15 13:18:35 -0400

    Added train challenge files

--- jQuery and Ajax/index.js
+++ jQuery and Ajax/index.js
@@ -7,6 +7,7 @@ function init() {
     document.getElementById('btnJoke').addEventListener('click', getJoke, false);
     document.getElementById('btnQuery').addEventListener('click', getNewJoke, false);
     document.getElementById('buttonZip').addEventListener('click', getWeather, false);
+    document.getElementById('btnTrain').addEventListener('click', getTrainInfo, false);
 }
 
 //-----------------Jokes---------------------
@@ -66,4 +67,37 @@ function showWeather(xml) {
     output += "Temperature: " + temperature + "<br/>";
     output += "Description: " + description + "<br/>";
     document.getElementById("resultWeather").innerHTML = output;
+}
+//-------------TRAINSSSSS------------------
+function getTrainInfo() {
+    $.ajax({
+        type: "GET",
+        url: "http://www3.septa.org/hackathon/Arrivals/90404/10",
+        dataType: "text",
+        success: function (output) {
+            showTrains(output);
+        }
+    });
+}
+
+function showTrains(result) {
+    var data = jQuery.parseJSON(result);
+
+    var arr = data[Object.keys(data)];
+    var northbound = arr[0].Northbound;
+    var southbound = arr[1].Southbound;
+
+    var output = "<img src='img/logo.gif'/><h1 id='north'>Northbound</h1><table class='northbound'>";
+    output += "<tr><th>ID:</th><th>Destination</th><th>Depart Time</th><th>Service Type</th><th>Status</th></tr>";
+    for (i = 0; i < northbound.length; i++) {
+        output += "<tr class='trainCell'><th>" + northbound[i].train_id + "</th><th>" + northbound[i].destination + "</th><th>" + northbound[i].depart_time + "</th><th>" + northbound[i].service_type + "</th><th>" + northbound[i].status + "</th></tr>";
+    }
+    output += "</table>";
+    output += "<h1 id='south'>Southbound</h1><table class='southbound'>";
+    output += "<tr><th>ID:</th><th>Destination</th><th>Depart Time</th><th>Service Type</th><th>Status</th></tr>";
+    for (i = 0; i < southbound.length; i++) {
+        output += "<tr class='trainCell'><th>" + southbound[i].train_id + "</th><th>" + southbound[i].destination + "</th><th>" + southbound[i].depart_time + "</th><th>" + southbound[i].service_type + "</th><th>" + southbound[i].status + "</th></tr>";
+    }
+    output += "</table>";
+    document.getElementById("trainInfo").innerHTML = output;
 }
