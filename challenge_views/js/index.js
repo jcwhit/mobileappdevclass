539a8485f42cdcf77f6920c5bfe53186894c345a
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2016-06-15 13:16:34 -0400

    Added data to challenge views

--- challenge_views/js/index.js
+++ challenge_views/js/index.js
@@ -1,3 +1,23 @@
+var myData = {
+    items: [
+        {
+            name: "James",
+            place: "France",
+            thing: "Books"
+        },
+        {
+            name: "Laura",
+            place: "Mexico",
+            thing: "Chairs"
+        },
+        {
+            name: "Sam",
+            place: "Italy",
+            thing: "Paper"
+        }
+    ]
+};
+
 function changeStyleOne() {
     var namesArray = document.getElementById('style');
     namesArray.setAttribute('href', 'css/index.css');
@@ -8,19 +28,19 @@ function changeStyleOne() {
         "<div class='thingcellOne'>Things</div>" +
         "</div>" +
         "<div class='row One'>" +
-        "<div class='namecellOne'>James</div>" +
-        "<div class='placecellOne'>France</div>" +
-        "<div class='thingcellOne'>Books</div>" +
+        "<div class='namecellOne'>" + myData.items[0].name + "</div>" +
+        "<div class='placecellOne'>" + myData.items[0].place + "</div>" +
+        "<div class='thingcellOne'>" + myData.items[0].thing + "</div>" +
         "</div>" +
         "<div class='row One'>" +
-        "<div class='namecellOne'>Laura</div>" +
-        "<div class='placecellOne'>Mexico</div>" +
-        "<div class='thingcellOne'>Chairs</div>" +
+        "<div class='namecellOne'>" + myData.items[1].name + "</div>" +
+        "<div class='placecellOne'>" + myData.items[1].place + "</div>" +
+        "<div class='thingcellOne'>" + myData.items[1].thing + "</div>" +
         "</div>" +
         "<div class='row One'>" +
-        "<div class='namecellOne'>Sam</div>" +
-        "<div class='placecellOne'>Italy</div>" +
-        "<div class='thingcellOne'>Paper</div>" +
+        "<div class='namecellOne'>" + myData.items[2].name + "</div>" +
+        "<div class='placecellOne'>" + myData.items[2].place + "</div>" +
+        "<div class='thingcellOne'>" + myData.items[2].thing + "</div>" +
         "</div>";
 }
 
@@ -30,17 +50,17 @@ function changeStyleTwo() {
     namesArray.setAttribute('href', 'css/indexTwo.css');
     var firstBox = document.getElementById('table');
     firstBox.innerHTML = "<div><ul id='box1' class='row tableheader One'>" +
-        "<li class='namecellOne'>Names:James</li>" +
-        "<li class='placecellOne'>Places:France</li>" +
-        "<li class='thingcellOne'>Things:Books</li></ul></div>" +
+        "<li class='namecellOne'>Names:" + myData.items[0].name + "</li>" +
+        "<li class='placecellOne'>Places:" + myData.items[0].place + "</li>" +
+        "<li class='thingcellOne'>Things:" + myData.items[0].thing + "</li></ul></div>" +
         "<div><ul id='box2' class='row tableheader One'>" +
-        "<li class='namecellOne'>Names:Laura</li>" +
-        "<li class='placecellOne'>Places:Mexico</li>" +
-        "<li class='thingcellOne'>Things:Chairs</li></ul></div>" +
+        "<li class='namecellOne'>Names:" + myData.items[1].name + "</li>" +
+        "<li class='placecellOne'>Places:" + myData.items[1].place + "</li>" +
+        "<li class='thingcellOne'>Things:" + myData.items[1].thing + "</li></ul></div>" +
         "<div><ul id='box3' class='row tableheader One'>" +
-        "<li class='namecellOne'>Names:Sam</li>" +
-        "<li class='placecellOne'>Places:Italy</li>" +
-        "<li class='thingcellOne'>Things:Paper</li></ul></div>";
+        "<li class='namecellOne'>Names:" + myData.items[2].name + "</li>" +
+        "<li class='placecellOne'>Places:" + myData.items[2].place + "</li>" +
+        "<li class='thingcellOne'>Things:" + myData.items[2].thing + "</li></ul></div>";
 
 }
 
@@ -49,23 +69,23 @@ function changeStyleThree() {
     namesArray.setAttribute('href', 'css/indexThree.css');
     var box = document.getElementById('table');
     box.innerHTML = "<div><ul id='box' class='row'>" +
-        "<li class='first name' onclick=" + "displayInfo('James');" + ">James</li>" +
-        "<li class='first name' onclick=" + "displayInfo('Laura');" + ">Laura</li>" +
-        "<li class='first name' onclick=" + "displayInfo('Sam');" + ">Sam</li>" +
+        "<li class='first name' onclick=" + "displayInfo('James');" + ">" + myData.items[0].name + "</li>" +
+        "<li class='first name' onclick=" + "displayInfo('Laura');" + ">" + myData.items[1].name + "</li>" +
+        "<li class='first name' onclick=" + "displayInfo('Sam');" + ">" + myData.items[2].name + "</li>" +
         "</ul></div>";
 }
 
 function displayInfo(info) {
     if (info == 'James') {
         var disp = document.getElementById("display");
-        disp.innerHTML = "James-France-Books";
+        disp.innerHTML = myData.items[0].name + '-' + myData.items[0].place + '-' + myData.items[0].thing;
     }
     if (info == 'Laura') {
         var dispTwo = document.getElementById("display");
-        dispTwo.innerHTML = "Laura-Mexico-Chairs";
+        dispTwo.innerHTML = myData.items[1].name + '-' + myData.items[1].place + '-' + myData.items[1].thing;
     }
     if (info == 'Sam') {
         var dispThree = document.getElementById("display");
-        dispThree.innerHTML = "Sam-Italy-Paper";
+        dispThree.innerHTML = myData.items[2].name + '-' + myData.items[2].place + '-' + myData.items[2].thing;
     }
 }
