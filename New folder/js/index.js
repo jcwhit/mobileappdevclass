18fa5ac49a39f19b0466c3e2248408924f531654
Jackson Whitley <jackson.whitley@bobcats.gcsu.edu>
2016-06-30 11:27:42 -0400

    Added a bunch of class projects

--- New folder/js/index.js
+++ New folder/js/index.js
@@ -1,9 +1,72 @@
 var myData4 = {
-items:[
-{id:"1 2 3", fName:"abe", lName:"lincoln", job:"president"},
-{id:"2 0 0", fName:"brad ", lName:"barfield", job:"vice president"},
-{id:"3 0 1", fName:"cody ", lName:"nash", job:"secretary"},
-{id:"4 4 4", fName:"edgar", lName:"raven", job:"treasurer"},
-{id:"5 1 0", fName:"franks", lName:"in burger", job:"swan"}
+    items: [
+        {
+            id: "1 2 3",
+            fName: "abe",
+            lName: "lincoln",
+            job: "president"
+        },
+        {
+            id: "2 0 0",
+            fName: "brad ",
+            lName: "barfield",
+            job: "vice president"
+        },
+        {
+            id: "3 0 1",
+            fName: "cody ",
+            lName: "nash",
+            job: "secretary"
+        },
+        {
+            id: "4 4 4",
+            fName: "edgar",
+            lName: "raven",
+            job: "treasurer"
+        },
+        {
+            id: "5 1 0",
+            fName: "franks",
+            lName: "in burger",
+            job: "swan"
+        }
 ]
 };
+
+window.onload = function () {
+    showMyList();
+}
+
+
+function clearTags() {
+    var arr = document.getElementsByTagName("span");
+    for (i = 0; i < arr.length; i++) {
+        arr[i].classList.remove("block", "hidden", "vhidden", "border");
+    }
+}
+
+function showTable() {
+    clearTags();
+    var table = document.getElementsByClassName("table");
+    var rows = document.getElementsByClassName("row");
+    var cells = document.getElementsByClassName("cell");
+
+    for (i = 0; i < cells.length; i++) {
+        cells[i].classList.add("border");
+    }
+}
+
+function showMyList() {
+    clearTags();
+    var rows = document.getElementsByClassName("row");
+
+    for (r = 0; rows.length; r++) {
+        rows[r].classList.add("block", "border");
+
+        var cells = rows[r].getElementsByClassName("cell");
+        
+        for (i = 0; i < cells.length; i++) {
+            cells[i].classList.add("block");
+        }
+    }
+}
